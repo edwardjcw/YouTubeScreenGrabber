5135ced9da220645f49b5e4784e09c449486b259
edward-vite <edward-vite@DESKTOP-2VMFAU2>
2017-03-12 22:08:00 -0500

    Almost got it to work using VLC

---
+++ YouTubeScreenGrabber/YouTubeScreenGrabber/Helper.fs
@@ -0,0 +1,74 @@
+﻿namespace Helper 
+
+open System.Drawing
+open System.Web
+open FSharp.Data
+open System.Xml
+open System.Xml.Linq
+open System.Windows.Forms
+open System.IO
+open System
+
+type Video = {url:string; start:int; stop:int}
+
+type Link = 
+    {original:string; embeded:Video; image:Bitmap option}
+
+type Helper() =
+
+    static let extractNameAndTime (uri : string) =
+        let slash = '\\'
+        let times = [|"?t=";"?start="|]
+        uri.Split([|slash|])
+        |> Array.last
+        |> (fun x -> x.Split(times, System.StringSplitOptions.None))
+        |> (fun x -> (x.[0], x |> Array.last |> int))
+        |> (fun (u, s) -> {url = u; start = s; stop = s+1})
+
+    static let transformToImage (video : Video) =
+ 
+        let vlc = @"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+        let tempDir = 
+            (Path.GetTempPath(), Guid.NewGuid().ToString())
+            |> Path.Combine
+            |> Path.GetFullPath
+        let settings start stop path = @" --video-filter=scene --vout=dummy --no-audio --start-time=" + (start |> string) + @" --stop-time=" + (stop |> string) + @" --scene-format=png --scene-path=" + path
+        //let quit = " vlc://quit"
+
+        use myProcess = new System.Diagnostics.Process()
+        myProcess.StartInfo.UseShellExecute <- false
+        myProcess.StartInfo.FileName <- vlc
+        myProcess.StartInfo.Arguments <- video.url + (settings video.start video.stop tempDir) //+ quit
+        myProcess.StartInfo.CreateNoWindow <- true        
+        let started = 
+            try
+                myProcess.Start()
+            with | ex ->
+                ex.Data.Add("url", video.url)
+                reraise()
+        if not started then 
+            failwith "Failed to start process %s" video.url
+            
+        printfn "Started %s with pid %i" myProcess.ProcessName myProcess.Id
+        myProcess.WaitForExit()
+
+        tempDir
+        |> Directory.GetFiles
+        |> Array.last
+        |> (fun x -> Some(new Bitmap(x)))
+     
+    static let emptyVideo = {url=String.Empty; start=0; stop=0}
+        
+    static member BuildLink uri = 
+        {original=uri; embeded=emptyVideo; image=None}
+
+    static member BuildEmbedLink (link : Link) = 
+        {link with embeded = extractNameAndTime link.original}
+
+    static member BuildImage (link : Link) =
+        {link with image = transformToImage link.embeded}
+
+    static member SaveImage savePath (link : Link) =
+        match link.image with
+        | Some(i) -> i.Save(savePath)
+        | _ -> failwith "no image"
---
+++ YouTubeScreenGrabber/YouTubeScreenGrabber/Program.fs
@@ -0,0 +1,44 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Helper
+
+[<EntryPoint>]
+let main argv = 
+
+    match argv.[0].ToLower() with
+    | "-h" -> //show help
+        let help =
+            "YouTubeScreenGrabber:\n\
+            USAGE: YouTubeScreenGrabber -x [params], where x is the use code\n\
+            NOTE: All links MUST include a time marker or the program skips it.\n\
+            The program saves all images into the subdirectory Images. If the\n\
+            program converts more than one link, it saves a csv file in\n\
+            the root directory that maps the link to the saved image name.\n\
+            Convert one link to image: \n\
+            \t YouTubeScreenGrabber -o [link] [directory to save image]\n\
+            Convert multiple links to image: \n\
+            \t YouTubeScreenGranner -m [file path of new line delimited links] [save directory]"
+        help |> printfn "%A" |> ignore
+    | "-o" ->
+        let link = argv.[1]
+        let savePath = argv.[2]
+        link
+        |> Helper.BuildLink
+        |> Helper.BuildEmbedLink
+        |> Helper.BuildImage
+        |> Helper.SaveImage savePath
+        printfn "save complete" 
+        |> ignore
+//    | "-m" ->
+//        let links = extractLinks argv.[1]
+//        let savePath = argv.[2]
+//        links
+//        |> List.map buildEmbedLink
+//        |> List.map buildImage
+//        |> saveMultipleImages savePath
+//        |> printfn "save multiple images done"
+//        |> ignore 
+    | _ -> printfn "unknown args" |> ignore
+
+    0 // return an integer exit code
